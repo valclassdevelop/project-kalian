f68185b4482aa446ca82e25c2a9a940be363ebf0
valclassdevelop <79193720+valclassdevelop@users.noreply.github.com>
2021-02-27 14:48:57 +0700

    Delete property directory

--- property/backToTop.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.v-back').addEventListener('click', () => {
-    document.querySelector('.parentBackToTop').scrollTop = 0; // For Safari
-    document.querySelector('.parentBackToTop').scrollTop = 0  // For Chrome, Firefox, IE dan Opera
-})
--- property/reload.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-
-    for (var i = 0; i <= document.querySelectorAll('button.v-reload-web').length; i++) {
-        document.querySelectorAll('button.v-reload-web')[i].addEventListener('click', () => {
-           window.location.reload()
-        })
-    }
-
-}, 300)
